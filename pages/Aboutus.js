bac2aa1b7fba427e2e1e6159ea313e23840168c4
Ketanpatel0306 <Ketan_patel@codenticsoftware.com>
2022-03-25 23:29:23 +0530

    complete aboutus page

--- pages/Aboutus.js
+++ pages/Aboutus.js
@@ -1,11 +1,27 @@
-import { Bottombar, Header, Hidecomp } from "../components";
+import {
+  Banner2,
+  Benefits,
+  Bottombar,
+  Footer,
+  Header,
+  OurMissinon,
+  Process,
+  Teammember,
+  Whoweare,
+} from "../components";
 
 export default function Aboutus() {
   return (
     <div>
       <Header />
-      {/* <Hidecomp /> */}
+      <Banner2 />
+      <Whoweare />
+      <Process />
+      <OurMissinon />
+      <Benefits />
+      <Teammember />
       <Bottombar />
+      <Footer />
     </div>
   );
 }
